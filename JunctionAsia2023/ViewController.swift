e26824891c6affd967617cecf3cff91c567bc79d
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2023-08-18 16:54:56 +0900

    Initial Commit

---
+++ JunctionAsia2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JunctionAsia2023
+//
+//  Created by 최지철 on 2023/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
