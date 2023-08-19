1f8d286ecb89c83b55eaf66f77c8505857fa182f
Juny0110 <wnsdyds403@naver.com>
2023-08-19 13:44:48 +0900

    Add: UserDataViewContoller Layout

---
+++ JunctionAsia2023/Views/UserDataCell.swift
@@ -0,0 +1,83 @@
+//
+//  UserDataCell.swift
+//  JunctionAsia2023
+//
+//  Created by 지준용 on 2023/08/19.
+//
+
+import UIKit
+
+class UserDataCell: UICollectionViewCell {
+    
+    // MARK: - Enum
+    
+    private enum Size {
+        static let keywordLabelHeight: CGFloat = 44
+        static let keywordLabelXInset: CGFloat = 10
+    }
+    
+    // MARK: - Property
+    
+    static let identifier = "userDataCell"
+    lazy var keywordType: KeywordType = .previewKeyword
+    override var isSelected: Bool {
+        didSet {
+            if isSelected {
+                allergyLabel.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+                allergyLabel.textColor = .white
+                allergyLabel.backgroundColor = .blue
+            } else {
+                allergyLabel.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+                allergyLabel.textColor = .black
+                allergyLabel.backgroundColor = .systemGray5
+            }
+        }
+    }
+
+    // MARK: - View
+    
+    let allergyLabel = UILabel()
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Method
+
+    func layout() {
+        contentView.addSubview(allergyLabel)
+        allergyLabel.snp.makeConstraints {
+            $0.edges.equalTo(safeAreaLayoutGuide)
+            $0.edges.equalToSuperview().inset(27)
+        }
+    }
+
+    func configureText(_ allergy: String) {
+        allergyLabel.text = allergy
+    }
+    
+    func configureLabel(_ type: KeywordType) {
+        allergyLabel.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        allergyLabel.textColor = .black
+        allergyLabel.backgroundColor = .systemGray5
+        allergyLabel.textAlignment = .center
+        allergyLabel.layer.cornerRadius = Size.keywordLabelHeight / 2
+        allergyLabel.layer.masksToBounds = true
+        allergyLabel.layer.maskedCorners = type.maskedCorners
+    }
+    
+    static func fittingSize(availableHeight: CGFloat, _ allergyType: String) -> CGSize {
+        let cell = UserDataCell()
+        cell.configureText(allergyType)
+        let targetSize = CGSize(width: UIView.layoutFittingCompressedSize.width, height: availableHeight)
+        return cell.contentView.systemLayoutSizeFitting(targetSize, withHorizontalFittingPriority: .fittingSizeLevel, verticalFittingPriority: .required)
+    }
+}
